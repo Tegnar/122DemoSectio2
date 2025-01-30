22216d0e8f02ac36c9fa8f0656c2f05245e42288
Tegnar <NagoMinecraft@Gmail.com>
2025-01-30 17:44:29 -0600

    SpringProgram Commit
    Commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SpringProgram1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student aStudent1 = new Student();
+            Student aStudent2 = new Student();
+            Student aStudent3 = new Student();
+            Student aStudent4 = new Student(4, "Qasai", "Mohammed", "", "MSIT");
+            Student aStudent5 = new Student(5, "Jane", "Stevenson", "Patricia", "Dental Hygiene");
+
+            aStudent1.StudentId = 1;
+            aStudent1.FirstName = "Anna";
+            aStudent1.LastName = "Romanov";
+            aStudent1.MiddleName = "Marie";
+            aStudent1.Major = "Biology";
+
+            aStudent2.StudentId = 2;
+            aStudent2.FirstName = "Steven";
+            aStudent2.LastName = "Trotter";
+            aStudent2.MiddleName = "";
+            aStudent2.Major = "French";
+
+            aStudent3.StudentId = 3;
+            aStudent3.FirstName = "Omar";
+            aStudent3.LastName = "Mohanned";
+            aStudent3.MiddleName = "Qasai";
+            aStudent3.Major = "Computer";
+
+            List<Student> aListOfStudents = new List<Student>();
+            aListOfStudents.Add(aStudent1);
+            aListOfStudents.Add(aStudent2);
+            aListOfStudents.Add(aStudent3);
+            aListOfStudents.Add(aStudent4);
+            aListOfStudents.Add(aStudent5);
+
+            /*
+            Console.WriteLine(aStudent1);
+            Console.WriteLine(aStudent2);
+            Console.WriteLine(aStudent3);
+            Console.WriteLine(aStudent4);
+            Console.WriteLine(aStudent5);
+            */
+
+
+            foreach (Student aStudent in aListOfStudents)
+            {
+                Console.WriteLine(aStudent.ToString());
+            }    
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,98 @@
+﻿// Written by Ben Wilkins
+//1/15/2025 Start Date
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SpringProgram1
+{
+
+    public class Student
+    {
+        //class variables in camel case
+        private int studentId = -1;
+        private string firstName = "Some error: FName";
+        private string middleName = "Some error: MName";
+        private string lastName = "Some error: LName";
+        private string major = "Some error: Majorly";
+
+        //GPA is a calculated field so later in the semester 
+        //private double gpa = 0.0;
+
+        // get and sets Accessor Methods = gets and Mutator Methods = sets
+
+        public string FirstName
+        {
+            // This is a very bare bones get and set
+            // there could be a lot of code here
+            // but this is week 0 so there isn't a lot of code here yet
+            // the code would be for checking ranges and such
+            get { return this.firstName; }
+            set { this.firstName = value; }
+
+        }
+        public string MiddleName
+        {
+            get { return this.middleName; }
+            set { this.middleName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        public string Major
+        {
+            get { return this.major; }
+            set { this.major = value; }
+        }
+        public int StudentId
+        {
+            get { return this.studentId; }
+            set { this.studentId = value; }
+
+
+        }
+        //constructors
+
+        public Student()
+        {
+            // This is the empty constructor
+            //It is called the empty constructor because it has no parameters
+            // Normally this is given to you behind the scenes
+            // But once you make a constructor, the built in empty constructor disappears
+        }
+        public Student(int aStudentId, string aFirstName, string aLastName, string aMiddleName, string aMajor)
+        {
+            // This is called the Full Constructor
+            // Because it has one parameter for each property
+            // It constructs the entire thing
+            this.studentId  = aStudentId;
+            this.firstName  = aFirstName;
+            this.middleName = aMiddleName;
+            this.lastName   = aLastName;
+            this.major      = aMajor;
+
+        }
+        public Student(int aStudentId, string aFirstName, string aLastName, string aMiddleName):this()
+        {
+
+        }
+
+
+        //methods
+        public override string ToString()
+        {
+            string message = "";
+            message = message + "Student Id: "  + this.StudentId    + "\n";
+            message = message + "First Name: "  + this.firstName    + "\n";
+            message = message + "Middle Name: " + this.middleName   + "\n";
+            message = message + "Last Name: "   + this.lastName     + "\n";
+            message = message + "Major: "       + this.major        + "\n";
+            return message;
+        }
+    }
+}
